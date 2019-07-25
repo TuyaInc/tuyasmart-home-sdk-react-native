63023fd5ae8625296ff932b8b5c2a0ee5d924a71
wuww <wuww@tuya.com>
2019-07-25 16:38:24 +0800

    feat: security issues

--- ios/TuyaRnDemo/AppDelegate.m
+++ ios/TuyaRnDemo/AppDelegate.m
@@ -21,7 +21,7 @@
 //  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 //   [[TuyaSmartSDK sharedInstance] setDebugMode:YES];
 //   [[TuyaSmartSDK sharedInstance] startWithAppKey:@"" secretKey:@""];
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.21.246:8081/index.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios&dev=true"];
   
 //    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios&dev=true"];
 
